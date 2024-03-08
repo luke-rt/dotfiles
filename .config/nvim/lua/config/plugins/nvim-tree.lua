3889f8c38d134070a2b3f8fbb72b54dd71fe4d77
luke-rt <luke.rtong@gmail.com>
2024-03-08 15:50:35 -0500

    nvim tree

---
+++ .config/nvim/lua/config/plugins/nvim-tree.lua
@@ -0,0 +1,27 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    -- "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("nvim-tree").setup {
+      on_attach = function(bufnr)
+        local api = require "nvim-tree.api"
+
+        local function opts(desc)
+          return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+        end
+
+        api.config.mappings.default_on_attach(bufnr)
+        vim.keymap.set('n', '?', api.tree.toggle_help, opts('Help'))
+      end,
+      actions = {
+        open_file = {
+          -- quit_on_open = true,
+        },
+      },
+    }
+  end,
+}
